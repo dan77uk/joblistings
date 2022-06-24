e080eddb4eb9b7d3015f408b15fdf334b6105068
Daniel Phillips <dan.phillips100@gmail.com>
2022-06-24 12:46:56 +0100

    feat: conditonal render for new and featured jobs

--- src/components/Job.js
+++ src/components/Job.js
@@ -3,15 +3,20 @@ const Job = ({ data }) => {
   return (
     <li>
       <img src={data.logo} alt=''/>
-    <div>
-      {data.company}
-      {data.positon}
-      {data.postedAt}
-      {data.contract}
-      {data.location}
-    </div>
+      <div className="job-info">
+        <span className="company-name">{data.company}
+        {data.new ? 'new' : ''}
+        {data.featured ? 'featured' : ''}
 
+        </span>
 
+        <span className="job-position">{data.position}</span>
+        <span className="job-data">{data.postedAt}&#x2022;{data.contract}&#x2022;{data.location}</span>
+      </div>
+      <div className="tags">
+        hello
+
+      </div>
     </li>
   )
 }
