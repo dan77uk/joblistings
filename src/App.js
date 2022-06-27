b731caf235f751ddd2d04ca1e6fb392c4554f84f
Daniel Phillips <dan.phillips100@gmail.com>
2022-06-27 13:05:40 +0100

    chore: removed axios and json server from package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Job from './components/Job'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import { data } from './data'
 
 const App = () => {
@@ -8,15 +7,6 @@ const App = () => {
   const [jobs, setJobs] = useState([])
   const [filterCatagories, setFilterCatagories] = useState([])
 
-  // const hook = () => {
-  //   axios
-  //     .get('http://localhost:3001/jobs')
-  //     .then(response => {
-  //       setJobs(response.data)
-  //     })
-  // }
-
-  // useEffect(hook, [])
 
     const hook = () => {
       setJobs(data)
@@ -38,17 +28,6 @@ const App = () => {
     ))
   }
 
-<<<<<<< HEAD
-=======
-  let filteredList = []
-  filterCatagories.forEach(catagory => {
-    const test = jobs.filter(job => {
-      return job.languages.includes(catagory)
-    })
-    console.log(test);
-  })
-
->>>>>>> 10685471aa6303886601a27df2fdfe1e5a826fc7
 
   const removeCatagory = (event) => {
     setFilterCatagories(filterCatagories.filter(item => item !== event.target.attributes.value.value))
