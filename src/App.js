e226fa6437afa603c33ce6913d6d57f09c60a094
Daniel Phillips <dan.phillips100@gmail.com>
2022-06-28 12:03:34 +0100

    feat: keyword filter now working and rendering properly

--- src/App.js
+++ src/App.js
@@ -5,33 +5,32 @@ import { data } from './data'
 const App = () => {
 
   const [jobs, setJobs] = useState([])
-  const [filterCatagories, setFilterCatagories] = useState([])
+  const [filterCategories, setFilterCategories] = useState([])
 
-
-    const hook = () => {
-      setJobs(data)
-    }
-
-  useEffect(hook, [])
-
-  const tabValue = async (event) => {
-    if (filterCatagories.includes(event.target.textContent)) {
-      return 
-    }
-    setFilterCatagories(filterCatagories => [...filterCatagories, event.target.textContent ])
-    // setFilterCatagories(filterCatagories.concat(event.target.textContent ))
-
-    setJobs(jobs.filter((job) => 
-      filterCatagories.every((catagory) => 
-        Object.values(job).includes(catagory)
-      )
-    ))
+  const hook = () => {
+    setJobs(data)
   }
 
+  useEffect(hook, [jobs, filterCategories])
+
+  const tabValue = (event) => {
+    if (filterCategories.includes(event.target.textContent)) { return }
+    setFilterCategories(filterCategories.concat(event.target.textContent ))  
+  }
+
+  const res = jobs.filter((job) => 
+    filterCategories.every((catagory) => 
+      Object.values(job).includes(catagory) || Object.values(job.tools).includes(catagory) || Object.values(job.languages).includes(catagory)
+    )
+  )
 
   const removeCatagory = (event) => {
-    setFilterCatagories(filterCatagories.filter(item => item !== event.target.attributes.value.value))
-    
+    setFilterCategories(filterCategories.filter(item => item !== event.target.attributes.value.value))
+  }
+
+  const resetFilters = () => {
+    setJobs(jobs)
+    setFilterCategories([])
   }
 
   return (
@@ -39,18 +38,18 @@ const App = () => {
           <section className='header'></section>
 
     <div className="wrapper">
-      {filterCatagories.length > 0 ? 
+      {filterCategories.length > 0 ? 
         <section className='filter-catagories'>
           <ul>
-            {filterCatagories.map((catagory, index) =>
+            {filterCategories.map((catagory, index) =>
               <li key={index}>{catagory}
                 <span value={catagory} onClick={removeCatagory}></span>
               </li>)}
           </ul>
-          <button onClick={() => setFilterCatagories([])} className='clear-filters'>Clear</button>
+          <button onClick={resetFilters} className='clear-filters'>Clear</button>
         </section> : ''}
       <section className='job-listing'>
-        {jobs.map(job => 
+        {res.map(job => 
             <Job key={job.id} data={job} filterEvent={tabValue}/>
           )}
       </section>
